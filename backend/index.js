cf9ba4cd36607e592a5c11fc5bc13006b0d701da
alexfatkhi <146102287+alexfatkhi@users.noreply.github.com>
2025-02-09 15:29:30 +0700

    refactor Node.js with express router

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,12 @@ import express, { request, response } from "express"; //express: Mengimpor Expre
 import { PORT, mongoDBURL } from "./config.js"; //Mengimpor nilai PORT dari file config.js
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js"; //Apapun yang kamu tulis setelah export default akan diambil ketika diimpor di file lain. bookstore itu router. bebas nulisnya, jadi bisa pilih tulis bookstore
 
-const app = express(); //Membuat instance dari Express.js yang akan digunakan untuk menangani request dan response.
+const app = express();
 
 // Middleware for parsing request body
-// Memungkinkan server untuk membaca data dalam format JSON dari request.
 app.use(express.json());
-
-// app.get(path, callback)
-//Jika pengguna mengakses /, maka:
-//  Request dicetak ke console.
-//  "okeeeee mantaapppppp" juga dicetak ke console.
-//  Mengembalikan status HTTP 234 dengan teks "Welcome to MERN Stack Tutorial".
 
 app.get("/", (request, response) => {
   console.log(request); //request: Objek yang berisi informasi tentang permintaan dari client.
@@ -21,104 +15,12 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome to MERN Stack Tutorial"); //status 234 akan muncul di bagian browser -> network
 });
 
-// Route for Save a new Book
-app.post("/books", async (request, response) => {
-  //Memeriksa apakah semua data (title, author, publishYear) ada.
-  //Jika ada yang kurang, mengembalikan HTTP 400 (Bad Request) dengan pesan error.
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: "send all required fields: title, author, publishYear",
-      });
-    }
+app.use("/books", booksRoute);
+// 👉 Baris kode ini berarti semua rute dalam booksRoute.js akan memiliki prefix /books.
+// Jadi, kalau di booksRoute.js ada rute /, maka di aplikasi utama akan menjadi /books.
+// 👉 disini index.js menghubungkan booksRoute.js
+// 👉 membuat aliran eksekusi berjalan dari index.js ke booksRoute.js.
 
-    //Membuat objek buku baru dari data yang dikirim pengguna.
-    //Book.create(newBook) digunakan untuk menyimpan ke MongoDB.
-    //Mengembalikan HTTP 201 (Created) dengan data buku yang baru disimpan.
-    const newBook = {
-      title: request.body.title,
-      author: request.body.author,
-      publishYear: request.body.publishYear,
-    };
-
-    const book = await Book.create(newBook);
-
-    return response.status(201).send(book);
-    //Jika terjadi error, server akan menangani dan mengembalikan HTTP 500 (Internal Server Error).
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//Route for Get All Books from database
-app.get("/books", async (request, response) => {
-  try {
-    const books = await Book.find({});
-    return response.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//Route for Get one Book from database by id
-app.get("/books/:id", async (request, response) => {
-  try {
-    const { id } = request.params;
-
-    const book = await Book.findById(id);
-
-    return response.status(200).json(book);
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//Route for Update a Book
-app.put("/books/:id", async (request, response) => {
-  try {
-    if (
-      !request.body.title ||
-      !request.body.author ||
-      !request.body.publishYear
-    ) {
-      return response.status(400).send({
-        message: "Send all required fields: title, author, publishYear",
-      });
-    }
-
-    const { id } = request.params;
-
-    const result = await Book.findByIdAndUpdate(id, request.body);
-
-    if (!result) {
-      return response.status(404).json({ message: "Book not found" });
-    }
-
-    return response.status(200).send({ message: "Book update successfully" });
-  } catch (error) {
-    console.log(error.message);
-    response.status(500).send({ message: error.message });
-  }
-});
-
-//Menghubungkan ke MongoDB menggunakan mongoose.connect(mongoDBURL).
-
-// Jika sukses:
-// Mencetak "App connected to database".
-// Menjalankan server Express.js pada port yang sudah ditentukan (PORT).
-
-// Jika gagal:
-// Error akan dicetak ke console.
 mongoose
   .connect(mongoDBURL)
   .then(() => {
